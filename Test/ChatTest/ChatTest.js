78305f16effdf81c690b5413761fe7725ca82d80
Mike Vegeto <ZECTBynmo@.(none)>
2012-07-05 10:37:52 -0400

    Added a chat box widget into the chat test, and hooked it up.

--- Test/ChatTest/ChatTest.js
+++ Test/ChatTest/ChatTest.js
@@ -7,9 +7,14 @@ var server = httpServerImpl.createNewServer( 1111, null ),
 	longPoll = longPollImpl.createNewLongPoll( server ),
 	chat = chatImpl.createNewChat( server, longPoll );
 	
+// Send common files to the cient
+server.addRequestHandler("/jquery-1.7.1.min.js", server.createFileHandler("../../Common/jquery-1.7.1.min.js"));
+server.addRequestHandler("/jquery.ui.chatbox.js", server.createFileHandler("../../Common/jquery.ui.chatbox.js"));
+server.addRequestHandler("/jquery.ui.chatbox.css", server.createFileHandler("../../Common/jquery.ui.chatbox.css"));
+server.addRequestHandler("/jquery-ui-1.8.21.custom.min.js", server.createFileHandler("../../Common/jquery-ui-1.8.21.custom.min.js"));
+	
 // Send HTML to client
 server.addRequestHandler("/", server.createFileHandler("index.html"));
 
 // Send scripts to client
-server.addRequestHandler("/jquery-1.7.1.min.js", server.createFileHandler("../../Common/jquery-1.7.1.min.js"));
 server.addRequestHandler("/LongPoll.js", server.createFileHandler("../../Client/LongPoll.js"));
