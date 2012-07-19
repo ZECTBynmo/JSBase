1cdc18d286f91d301372d7ccacbb2dc27def313e
ZECTBynmo <michael.vegeto@gmail.com>
2012-07-19 19:14:32 -0400

    Integrate PhantomJS into unit tests

--- Test/UnitTests/ChannelTests.js
+++ Test/UnitTests/ChannelTests.js
@@ -1,5 +1,6 @@
 var assert = require("assert");
 var httpMocks = require("node-mocks-http");
+var phantom = require('phantom');
 
 console.log( " " );
 console.log( "--------------------------------" );
@@ -21,20 +22,51 @@ var server = httpServerImpl.createNewServer( 1212, null ),
 	channel = new Channel( server );
 	
 // Serve our test file
-server.serveFile( "/", "./TestHTML/sendtest.html" );
+server.serveFile( "/", "./TestHTML/sendTestString.html" );
 server.serveFile( "/jquery-1.7.1.min.js", "../../Client/Common/jquery-1.7.1.min.js" );
 
+var hasRunTests = false;
 
 var runTests = function() {
+
 	describe('Channel', function() {
+		console.log( "RUNNING Channel TESTS" );
+		
 		it('Should be able to respond to an HTTP request', function( done ) {
 			var hasResponded = false;
 			
-			channel.on( "/test", function() {
+			// Tell the client what our test string is
+			server.addGenericHandler( "/whatTestString", function(data, respondToClient) { 
+				var responseData = {
+					testString: "/ChannelTests"
+				}
+				
+				if( !hasRunTests )
+					respondToClient( responseData );
+			});
+			
+			// Wait for the response
+			channel.on( "/ChannelTests", function() {
+				if( hasRunTests ) return;
+			
 				hasResponded = true;
 				assert.equal( true, true );
-				console.log( "ChannelTests: Callback from /test" );
+				console.log( "ChannelTests: Callback from /ChannelTests" );
 				done(); 
+			});
+			
+			// Load the page in a headless browser
+			phantom.create(function(ph) {
+				return ph.createPage(function(page) {
+					return page.open("http://localhost:1212/", function(status) {
+						console.log("opened the page? ", status);
+						
+						return page.evaluate( (function(){return document.title;}), function(result) {
+							console.log('Page title is ' + result);
+							return ph.exit();
+						});
+					});
+				});
 			});
 		});
 	});
--- Test/UnitTests/HTTPServerTests.js
+++ Test/UnitTests/HTTPServerTests.js
@@ -1,1 +1,33 @@
-var assert = require("assert");var httpMocks = require("node-mocks-http");var phantom;phantom = require('phantom');phantom.create(function(ph) {	return ph.createPage(function(page) {		return page.open("http://www.google.com", function(status) {			console.log("opened google? ", status);						return page.evaluate( (function(){return document.title;}), function(result) {				console.log('Page title is ' + result);				return ph.exit();			});		});	});});console.log( " " );console.log( "--------------------------------" );console.log( "HTTPServer Tests" );console.log( "--------------------------------" );var http = require("http");var serverImpl = require("../../Server/HTTPServer");var httpServer = serverImpl.createNewServer( 1212, null );// Serve our test pagehttpServer.serveFile( "/", "./TestHTML/sendtest.html" );httpServer.serveFile( "/jquery-1.7.1.min.js", "../../Client/Common/jquery-1.7.1.min.js" );var runTests = function() {	describe('HTTPServer', function() {		it('Should be able to respond to an HTTP request', function( done ) {			var hasResponded = false;						httpServer.on( "/test", function() {				hasResponded = true;				assert.equal( true, hasResponded );				console.log( "HTTPServerTests: Callback from /test" );				done();			});		});	});}; // end runTests();exports.runTests = runTests;
+var assert = require("assert");var httpMocks = require("node-mocks-http");var phantom = require('phantom');console.log( " " );console.log( "--------------------------------" );console.log( "HTTPServer Tests" );console.log( "--------------------------------" );var http = require("http");var serverImpl = require("../../Server/HTTPServer");var httpServer = serverImpl.createNewServer( 1212, null );// Serve our test pagehttpServer.serveFile( "/", "./TestHTML/sendTestString.html" );httpServer.serveFile( "/jquery-1.7.1.min.js", "../../Client/Common/jquery-1.7.1.min.js" );
+
+var hasRunTests = false;
+var runTests = function() {	describe('HTTPServer', function() {
+		console.log( "RUNNING HTTPServer TESTS" );
+				it('Should be able to respond to an HTTP request', function( done ) {			var hasResponded = false;
+			
+			// Tell the client what our test string is
+			httpServer.addGenericHandler( "/whatTestString", function(data, respondToClient) { 
+				var responseData = {
+					testString: "/HTTPServerTests"
+				}
+			
+				if( !hasRunTests )
+					respondToClient( responseData );
+			});						httpServer.on( "/HTTPServerTests", function() {
+				if( hasRunTests ) return;
+							hasResponded = true;
+				hasRunTests = true;				assert.equal( true, hasResponded );				console.log( "HTTPServerTests: Callback from /HTTPServerTests" );				done();			});
+			
+			// Load the page in a headless browser to trigger our events
+			phantom.create(function(ph) {
+				return ph.createPage(function(page) {
+					return page.open("http://localhost:1212/", function(status) {
+						console.log("opened the page? ", status);
+						
+						return page.evaluate( (function(){return document.title;}), function(result) {
+							console.log('Page title is ' + result);
+							return ph.exit();
+						});
+					});
+				});
+			});		});	});}; // end runTests();exports.runTests = runTests;
