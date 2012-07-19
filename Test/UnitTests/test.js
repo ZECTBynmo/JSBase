2b40cd82e91cd600702e46eeafe63ac612a374f6
ZECTBynmo <michael.vegeto@gmail.com>
2012-07-19 19:15:00 -0400

    Swapping unit tests around

--- Test/UnitTests/test.js
+++ Test/UnitTests/test.js
@@ -2,5 +2,5 @@ var assert = require("assert");
 
 // Run our tests!
 require("./EventHandlerTests").runTests();
-require("./HTTPServerTests").runTests();
-//require("./ChannelTests").runTests();
+//require("./HTTPServerTests").runTests();
+require("./ChannelTests").runTests();
