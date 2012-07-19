457979d441b6707843ec96b2642f3a6029dc7ae5
Mike Vegeto <michael.vegeto@gmail.com>
2012-07-18 22:02:42 -0400

    Try out phantomjs in the http tests

--- Test/UnitTests/test.js
+++ Test/UnitTests/test.js
@@ -3,4 +3,4 @@ var assert = require("assert");
 // Run our tests!
 require("./EventHandlerTests").runTests();
 require("./HTTPServerTests").runTests();
-require("./ChannelTests").runTests();
+//require("./ChannelTests").runTests();
