a212121c4307865323c2d0693ddab53db6ba6a31
Mike Vegeto <michael.vegeto@gmail.com>
2012-07-19 20:14:04 -0400

    Loop through tests

--- Test/UnitTests/test.js
+++ Test/UnitTests/test.js
@@ -1,6 +1,14 @@
 var assert = require("assert");
 
-// Run our tests!
-require("./EventHandlerTests").runTests();
-//require("./HTTPServerTests").runTests();
-require("./ChannelTests").runTests();
+// Get our tests
+var moduleTestCollection = new Array();
+
+moduleTestCollection.push( "./EventHandlerTests" );
+moduleTestCollection.push( "./HTTPServerTests" );
+moduleTestCollection.push( "./ChannelTests" );
+
+for( var iTest = 0; iTest<moduleTestCollection.length; ++iTest ) {
+	var moduleTests = require( moduleTestCollection[iTest] );
+	
+	moduleTests.runTests();
+}
